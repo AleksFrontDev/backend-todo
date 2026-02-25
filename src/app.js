edd0969ba279c1e2ab26e1bfc2b1aed90d28ed81
Aleksandr <generalovshura9@gmail.com>
2026-02-25 11:19:00 +0300

    added healthz route

--- src/app.js
+++ src/app.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/healthz', (req, res) => {
+  res.status(200).send('OK');
+});
+
 //Routes
 app.use('/tasks', taskRoutes);
 app.use('/auth', authRoutes);
