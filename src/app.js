6f8aad30ec986579fabcaa1bfe821d6310539981
Aleksandr <generalovshura9@gmail.com>
2026-04-12 21:43:51 +0300

    made logger middleware

--- src/app.js
+++ src/app.js
@@ -3,11 +3,13 @@ const express = require('express');
 const cors = require('cors');
 const taskRoutes = require('./routes/tasks');
 const authRoutes = require('./routes/auth.routes');
+const logger = require('./middleware/logger.middleware');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(logger);
 
 app.get('/healthz', (req, res) => {
   res.status(200).send('OK');
